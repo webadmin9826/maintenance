b2d3c5e8c4a6553886a3c8db4a65261443c7eb08
webadmin9826 <151479946+webadmin9826@users.noreply.github.com>
2025-09-29 13:52:42 +0800

    Add files via upload

---
+++ api/lib/mongo.js
@@ -0,0 +1,21 @@
+// api/lib/mongo.js
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('Missing MONGODB_URI environment variable');
+}
+
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, {
+    serverSelectionTimeoutMS: 30000,
+    maxPoolSize: 10,
+  });
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+module.exports = clientPromise;
